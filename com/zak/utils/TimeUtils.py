af0d4f0b64e4d44fe2834da32ff2c97e597362a9
obiecana <lanjingl@outlook.com>
2019-04-15 01:36:59 +0800

    0时直接返回00:00

--- com/zak/utils/TimeUtils.py
+++ com/zak/utils/TimeUtils.py
@@ -2,6 +2,9 @@ class TimeUtils:
 
     @staticmethod
     def second2minute(second):
+        second = int(second)
+        if second <= 0:
+            return "00:00"
         m = int(second / 60)
         s = second - m * 60
         if m < 10:
