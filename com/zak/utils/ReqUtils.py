09da17080d8d366d4de4ec536f31dcb78f0e5a29
obiecana <lanjingl@outlook.com>
2019-04-15 01:36:37 +0800

    增加qq搜索

--- com/zak/utils/ReqUtils.py
+++ com/zak/utils/ReqUtils.py
@@ -27,14 +27,72 @@ class ReqUtils:
     # 歌词搜索: type = lrc
     _NETEASE_BASE_SEARCH = "https://api.itooi.cn/music/netease/search?key=579621905"
 
+    _TENCENT_BASE_SEARCH = "https://api.itooi.cn/music/tencent/search?key=579621905"
+
+    # 整合qq和网易云的搜索结果
     @staticmethod
-    def search_music(music_tag):
+    def search_music(music_tag, exact=False):
         logging.debug("搜索 %s" % music_tag)
-        return ReqUtils._search_music_by_name(music_tag)
+        netease_result = ReqUtils.netease_search_music(music_tag, exact)
+        tencent_result = ReqUtils.tencent_search_music(music_tag, exact)
+        new_result = list()
+        tmp = set()
+        len_n = len(netease_result)
+        len_t = len(tencent_result)
+        len_max = max(len_n, len_t)
+        for i in range(len_max - 1):
+            if i < len_n:
+                str_n = "name:%s|singer:%s" % (netease_result[i]["name"], netease_result[i]["singer"])
+            else:
+                str_n = None
+
+            if i < len_n:
+                str_t = "name:%s|singer:%s" % (tencent_result[i]["name"], tencent_result[i]["singer"])
+            else:
+                str_t = None
+
+            if str_n is None:
+                new_result.append(tencent_result[i])
+                tmp.add(str_t)
+            elif str_t is None:
+                new_result.append(netease_result[i])
+                tmp.add(str_n)
+            else:
+                new_result.append(netease_result[i])
+                tmp.add(str_n)
+                if str_t not in tmp:
+                    new_result.append(tencent_result[i])
+
+        # tmp_ = set()
+        # new_result_ = list()
+        # for v in netease_result:
+        #     tmp_.add("name:%s|singer:%s" % (v["name"], v["singer"]))
+        #     new_result_.append(v)
+        #
+        # for v in tencent_result:
+        #     str_ = "name:%s|singer:%s" % (v["name"], v["singer"])
+        #     if str_ not in tmp_:
+        #         new_result_.append(v)
+        #
+        # # TODO 去重
+        # print(len(new_result))
+        # print(len(new_result_))
+        return new_result
 
     @staticmethod
-    def _search_music_by_name(music_name):
-        url = ReqUtils._NETEASE_BASE_SEARCH + "&s=" + music_name
+    def netease_search_music(music_tag, exact=False):
+        return ReqUtils._search_music_by_name(music_tag, ReqUtils._NETEASE_BASE_SEARCH, exact)
+        pass
+
+    @staticmethod
+    def tencent_search_music(music_tag, exact=False):
+        return ReqUtils._search_music_by_name(music_tag, ReqUtils._TENCENT_BASE_SEARCH, exact)
+
+    @staticmethod
+    def _search_music_by_name(music_name, base_search, exact=False):
+        url = base_search + "&s=" + music_name
+        if exact:
+            url += "&type=song"
         logging.debug("搜索 url:%s" % url)
         get_result = requests.get(url)
         json_result = json.loads(get_result.content.decode())
@@ -57,10 +115,15 @@ class ReqUtils:
             with open(filename, "wb") as tmp:
                 tmp.write(result.content)
             logging.debug("下载完成[%s]" % filename)
-        # 自动判断图片的类型
-        img_type = imghdr.what(filename)
-        if img_type is not None:
-            new_filename = filename[:-4] + "." + img_type
-            os.rename(filename, new_filename)
-            filename = new_filename
+            size = os.path.getsize(filename)
+            print(size)
+            # 自动判断图片的类型
+            img_type = imghdr.what(filename)
+            if img_type is not None:
+                new_filename = filename[:-4] + "." + img_type
+                try:
+                    os.rename(filename, new_filename)
+                except Exception as e:
+                    pass
+                filename = new_filename
         return filename
