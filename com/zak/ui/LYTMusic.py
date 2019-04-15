1628acf1961b69c271c6d4e57871440dddbe15cc
obiecana <lanjingl@outlook.com>
2019-04-16 00:56:14 +0800

    增加本地播放

--- com/zak/ui/LYTMusic.py
+++ com/zak/ui/LYTMusic.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QObject
 from PyQt5.QtCore import QTimer
 
 from com.zak.dao.MusicDao import MusicDao
+from com.zak.music.Music import Music
 from com.zak.music.Player import Player
 from com.zak.utils.Converter import Converter
 # Form implementation generated from reading ui file 'LYTMusic.ui'
@@ -68,22 +68,52 @@ class Ui_MainWindow(QObject):
         self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_3.setObjectName("verticalLayout_3")
         self.tabWidget = QtWidgets.QTabWidget(self.page)
-        self.tabWidget.setStyleSheet("QTabBar::tab{width:180px;} ")
+        self.tabWidget.setStyleSheet("QTabBar::tab{width:180px;}")
         self.tabWidget.setTabShape(QtWidgets.QTabWidget.Rounded)
         self.tabWidget.setElideMode(QtCore.Qt.ElideMiddle)
         self.tabWidget.setTabBarAutoHide(True)
         self.tabWidget.setObjectName("tabWidget")
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.tab)
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.stackedWidget_2 = QtWidgets.QStackedWidget(self.tab)
+        self.stackedWidget_2.setObjectName("stackedWidget_2")
+        self.page_3 = QtWidgets.QWidget()
+        self.page_3.setObjectName("page_3")
+        self.verticalLayout_6 = QtWidgets.QVBoxLayout(self.page_3)
+        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_6.setObjectName("verticalLayout_6")
+        self.local_list_widget = QtWidgets.QListWidget(self.page_3)
+        self.local_list_widget.setObjectName("local_list_widget")
+        self.verticalLayout_6.addWidget(self.local_list_widget)
+        self.stackedWidget_2.addWidget(self.page_3)
+        self.page_4 = QtWidgets.QWidget()
+        self.page_4.setObjectName("page_4")
+        self.stackedWidget_2.addWidget(self.page_4)
+        self.verticalLayout_5.addWidget(self.stackedWidget_2)
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
         self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.tab_2)
         self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_4.setObjectName("verticalLayout_4")
-        self.listWidget = QtWidgets.QListWidget(self.tab_2)
-        self.listWidget.setObjectName("listWidget")
-        self.verticalLayout_4.addWidget(self.listWidget)
+        self.stackedWidget_3 = QtWidgets.QStackedWidget(self.tab_2)
+        self.stackedWidget_3.setObjectName("stackedWidget_3")
+        self.page_5 = QtWidgets.QWidget()
+        self.page_5.setObjectName("page_5")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout(self.page_5)
+        self.verticalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
+        self.net_list_widget = QtWidgets.QListWidget(self.page_5)
+        self.net_list_widget.setObjectName("net_list_widget")
+        self.verticalLayout_7.addWidget(self.net_list_widget)
+        self.stackedWidget_3.addWidget(self.page_5)
+        self.page_6 = QtWidgets.QWidget()
+        self.page_6.setObjectName("page_6")
+        self.stackedWidget_3.addWidget(self.page_6)
+        self.verticalLayout_4.addWidget(self.stackedWidget_3)
         self.tabWidget.addTab(self.tab_2, "")
         self.verticalLayout_3.addWidget(self.tabWidget)
         self.stackedWidget.addWidget(self.page)
@@ -102,19 +132,33 @@ class Ui_MainWindow(QObject):
         self.pic_label.setMinimumSize(QtCore.QSize(50, 50))
         self.pic_label.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.pic_label.setAutoFillBackground(False)
+        self.pic_label.setText("")
         self.pic_label.setObjectName("pic_label")
         self.horizontalLayout.addWidget(self.pic_label)
+        spacerItem = QtWidgets.QSpacerItem(10, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
         self.song_label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("宋体")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setWeight(50)
+        self.song_label.setFont(font)
+        self.song_label.setStyleSheet("QLabel{color: black}")
         self.song_label.setObjectName("song_label")
         self.verticalLayout.addWidget(self.song_label)
         self.singer_label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("宋体")
+        self.singer_label.setFont(font)
+        self.singer_label.setStyleSheet("QLabel {color:gray}")
         self.singer_label.setObjectName("singer_label")
         self.verticalLayout.addWidget(self.singer_label)
         self.horizontalLayout.addLayout(self.verticalLayout)
         self.horizontalLayout.setStretch(0, 1)
-        self.horizontalLayout.setStretch(1, 3)
+        self.horizontalLayout.setStretch(2, 3)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
@@ -155,7 +199,8 @@ class Ui_MainWindow(QObject):
 
         self.volume_slider.valueChanged.connect(self.refresh_volume)
         self.input_search.returnPressed.connect(self.search_test)
-        self.listWidget.doubleClicked.connect(self.db_click)
+        self.net_list_widget.doubleClicked.connect(self.net_list_widget_double_click)
+        self.local_list_widget.doubleClicked.connect(self.local_list_widget_double_click)
         self._player.signal_start_play.connect(self.slot_music_start_play)
         self.mc_play_pause.clicked.connect(self.play_pause)
 
@@ -163,8 +208,9 @@ class Ui_MainWindow(QObject):
 
         self.retranslateUi(MainWindow)
         self.stackedWidget.setCurrentIndex(0)
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.__init_local_list_widget()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -181,7 +227,80 @@ class Ui_MainWindow(QObject):
         self.mc_play_pause.setText(_translate("MainWindow", "播放"))
         self.mc_next.setText(_translate("MainWindow", "下一曲"))
 
-    def test_list_item(self, index, music):
+
+    def search_test(self):
+        self.net_list_widget.clear()
+        title = self.input_search.text().title()
+        # "我喜欢上你内心时的活动"
+        sr = ReqUtils.search_music(title)
+        for i, v in enumerate(sr, 1):
+            Ui_MainWindow.__gen_list_item(self.net_list_widget, i, Converter.itooi_music(v))
+
+    def net_list_widget_double_click(self, index_):
+        self._player.stop()
+        self.__paint_default_pic()
+        item = self.net_list_widget.item(index_.row())
+        widget = self.net_list_widget.itemWidget(item)
+        music = widget.zak_music
+        music.signal_load_over.connect(self.__music_download_over)
+        # child = widget.findChild(QtWidgets.QLabel, "song_name_label")
+        # 开始播放
+        # music.get_uri()
+        self.full_music_time.setText(TimeUtils.second2minute(music.get_length()))
+        self.song_label.setText(music.get_name())
+        self.singer_label.setText(music.get_singer().replace("-", "/"))
+        self._player.play(music)
+
+    def local_list_widget_double_click(self, index_):
+        self._player.stop()
+        item = self.local_list_widget.item(index_.row())
+        widget = self.local_list_widget.itemWidget(item)
+        music = widget.zak_music
+        self.full_music_time.setText(TimeUtils.second2minute(music.get_length()))
+        self.song_label.setText(music.get_name())
+        self.singer_label.setText(music.get_singer().replace("-", "/"))
+        self._player.play(music)
+        pass
+
+    def slot_music_start_play(self, music):
+        # 刷新图片
+        pic = music.get_pic()
+        q_pixmap = QtGui.QPixmap(pic)
+        q_pixmap = q_pixmap.scaled(self.pic_label.size(), QtCore.Qt.KeepAspectRatio)
+        self.pic_label.setPixmap(q_pixmap)
+        # 设置总时长
+        self.music_progress.setMaximum(music.get_length() * 1000)
+        pass
+
+    def play_pause(self):
+        self._player.smart_pause()
+
+    def refresh_volume(self):
+        value = self.volume_slider.value()
+        self._player.set_volume(value)
+
+    # 刷新进度条
+    def __refresh_music_progress(self):
+        pos = self._player.get_pos()
+        self.curr_music_time.setText(TimeUtils.second2minute(pos / 1000))
+        self.music_progress.setValue(pos)
+
+    # 绘制默认图片
+    def __paint_default_pic(self):
+        q_pixmap = QtGui.QPixmap("./res/default.jpg").scaled(self.pic_label.size(), QtCore.Qt.KeepAspectRatio)
+        self.pic_label.setPixmap(q_pixmap)
+
+    def __music_download_over(self, music):
+        MusicDao.save(music)
+        pass
+
+    def __init_local_list_widget(self):
+        all = MusicDao.get_all()
+        for i, v in enumerate(all, 1):
+            Ui_MainWindow.__gen_list_item(self.local_list_widget, i, v)
+
+    @staticmethod
+    def __gen_list_item(list_widget: QtWidgets.QListWidget, index: int, music: Music):
         widget = QtWidgets.QWidget()
         mainArea = QtWidgets.QWidget()
 
@@ -225,63 +344,8 @@ class Ui_MainWindow(QObject):
         item = QtWidgets.QListWidgetItem()
         size = item.sizeHint()
         item.setSizeHint(QtCore.QSize(size.width(), 56))
-        self.listWidget.addItem(item)
+        list_widget.addItem(item)
 
         widget.setSizeIncrement(size.width(), 56)
         widget.zak_music = music
-        self.listWidget.setItemWidget(item, widget)
-
-    def search_test(self):
-        self.listWidget.clear()
-        title = self.input_search.text().title()
-        # "我喜欢上你内心时的活动"
-        sr = ReqUtils.search_music(title)
-        for i, v in enumerate(sr, 1):
-            self.test_list_item(i, Converter.itooi_music(v))
-
-    def db_click(self, index_):
-        self._player.stop()
-        self.__paint_default_pic()
-        item = self.listWidget.item(index_.row())
-        widget = self.listWidget.itemWidget(item)
-        music = widget.zak_music
-        music.signal_load_over.connect(self.__music_download_over)
-        # child = widget.findChild(QtWidgets.QLabel, "song_name_label")
-        # 开始播放
-        # music.get_uri()
-        self.full_music_time.setText(TimeUtils.second2minute(music.get_length()))
-        self.song_label.setText(music.get_name())
-        self.singer_label.setText(music.get_singer().replace("-", "/"))
-        self._player.play(music)
-
-    def slot_music_start_play(self, music):
-        # 刷新图片
-        pic = music.get_pic()
-        q_pixmap = QtGui.QPixmap(pic)
-        q_pixmap = q_pixmap.scaled(self.pic_label.size(), QtCore.Qt.KeepAspectRatio)
-        self.pic_label.setPixmap(q_pixmap)
-        # 设置总时长
-        self.music_progress.setMaximum(music.get_length() * 1000)
-        pass
-
-    def play_pause(self):
-        self._player.smart_pause()
-
-    def refresh_volume(self):
-        value = self.volume_slider.value()
-        self._player.set_volume(value)
-
-    # 刷新进度条
-    def __refresh_music_progress(self):
-        pos = self._player.get_pos()
-        self.curr_music_time.setText(TimeUtils.second2minute(pos / 1000))
-        self.music_progress.setValue(pos)
-
-    # 绘制默认图片
-    def __paint_default_pic(self):
-        q_pixmap = QtGui.QPixmap("./res/default.jpg").scaled(self.pic_label.size(), QtCore.Qt.KeepAspectRatio)
-        self.pic_label.setPixmap(q_pixmap)
-
-    def __music_download_over(self, music):
-        MusicDao.save(music)
-        pass
+        list_widget.setItemWidget(item, widget)
