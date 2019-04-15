0614fe458066c393e32464c7dfb8fc443b6acb83
obiecana <lanjingl@outlook.com>
2019-04-16 00:58:18 +0800

    setting dao

---
+++ com/zak/dao/SettingDao.py
@@ -0,0 +1,61 @@
+import logging
+
+from com.zak.utils.DBUtils import DBUtils
+
+
+class SettingDao:
+
+    @staticmethod
+    def set_volume(value: int):
+        SettingDao.__update("volume", value)
+        pass
+
+    @staticmethod
+    def set_last_pos(last_pos: int):
+        SettingDao.__update("last_pos", last_pos)
+        pass
+
+    @staticmethod
+    def set_last_music(music_id: int):
+        SettingDao.__update("last_music", music_id)
+
+    @staticmethod
+    def get_volume():
+        v = SettingDao.__get("volume")
+        if v is None:
+            return 0
+        else:
+            return int(v)
+
+    @staticmethod
+    def get_last_music():
+        v = SettingDao.__get("last_music")
+        if v is None:
+            return 0
+        else:
+            return int(v)
+
+    @staticmethod
+    def get_last_pos():
+        v = SettingDao.__get("last_pos")
+        if v is None:
+            return 0
+        else:
+            return int(v)
+
+    @staticmethod
+    def __get(name):
+        v_ = DBUtils.get_by_single("setting", "name", name)
+        try:
+            v = v_[2]
+            return v
+        except Exception as e:
+            logging.info(e)
+            return None
+
+    @staticmethod
+    def __update(name, value):
+        conn = DBUtils.get_connect()
+        sql = "update setting set value = '%s' where name = '%s'" % (str(value), name)
+        conn.execute(sql)
+        conn.commit()
