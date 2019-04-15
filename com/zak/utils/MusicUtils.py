58dccd675a82a38390491f080172e9c5a454c2ba
obiecana <lanjingl@outlook.com>
2019-04-15 23:13:45 +0800

    增加参数类型指定

--- com/zak/utils/MusicUtils.py
+++ com/zak/utils/MusicUtils.py
@@ -1,24 +1,10 @@
-import time
-
-import pygame
 from mutagen.mp3 import MP3
 
 
 class MusicUtils:
 
     @staticmethod
-    def play(file_name):
-        pygame.mixer.init()
-
-        length = MusicUtils.get_length(file_name)
-        pygame.mixer.music.load(file_name)
-
-        pygame.mixer.music.play()
-        time.sleep(length)
-        pygame.mixer.music.stop()
-
-    @staticmethod
-    def get_length(file_name):
+    def get_length(file_name: str):
         music = MP3(file_name)
         return music.info.length
         pass
